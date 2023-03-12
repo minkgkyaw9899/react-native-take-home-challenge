e3f9bef1c790b958ce8b1f53b1de02fa625d1e11
minkgkyaw9899 <minkgkyaw9899@gmail.com>
2023-03-12 20:21:02 +0630

    refactor: update logo

--- src/screens/shopScreen.tsx
+++ src/screens/shopScreen.tsx
@@ -1,26 +1,23 @@
 import React, {FC} from 'react';
 import {StyleSheet, View} from 'react-native';
-import {ShopScreenProps} from 'types/navigation/types';
 import {Appbar} from 'react-native-paper';
+import {ShopScreenProps} from 'types/navigation/types';
 import {ProductCardList} from 'components/ProductCardList.component';
 import {CartModal} from 'components/CartModal.component';
+import Logo from "assets/logo.svg"
 
 const ShopScreen: FC<ShopScreenProps> = () => {
   return (
     <View style={styles.root}>
-      <Appbar.Header elevated style={{backgroundColor: '#FFFFFF'}}>
+      <Appbar.Header elevated style={{backgroundColor: '#FFFFFF', height: 80}}>
         <Appbar.Content
-          titleStyle={{textAlign: 'center'}}
+          titleStyle={{textAlign: 'center', marginBottom: 20}}
           title="TCG Marketplace"
         />
+        <Logo style={{position: 'absolute', top: 20, left: 150}} width={100} height={100} />
       </Appbar.Header>
       <View
-        style={{
-          flex: 1,
-          zIndex: 2,
-          justifyContent: 'center',
-          alignItems: 'center',
-        }}>
+        style={styles.productContainer}>
         <ProductCardList />
         <CartModal />
       </View>
@@ -33,6 +30,12 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#F8F8F8',
   },
+  productContainer: {
+    flex: 1,
+    zIndex: 2,
+    justifyContent: 'center',
+    alignItems: 'center',
+  }
 });
 
 export default ShopScreen;
